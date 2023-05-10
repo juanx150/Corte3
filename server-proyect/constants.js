5391c4cf90f6cf0ec75c51a702d1f6719741ad22
Juanma <juanm.ramirezs@autonoma.com.co>
2023-05-09 22:12:48 -0500

    actuaizado

---
+++ server-proyect/constants.js
@@ -0,0 +1,18 @@
+const DB_HOST = "cluster0.meuiihj.mongodb.net";
+const DB_USER = "ju123";
+const DB_PASSWORD = "ju123";
+const DB_NAME = "prueba_db";
+
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+const JWT_SECRET_KEY = "acceso-sistema-2023";
+
+module.exports = {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  API_VERSION,
+  IP_SERVER,
+  JWT_SECRET_KEY,
+};
